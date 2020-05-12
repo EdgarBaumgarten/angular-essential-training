8c0029c648da3fa4d31bb7d0e4ebca9a778bb5fa
Edgar Baumgarten <cwfbaumg@avans.nl>
2020-05-12 14:33:22 +0200

    training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
+import { lookupListToken, lookupLists } from './providers';
+//import { MediaItemService } from './media-item.service';
 
 @NgModule({
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
@@ -23,6 +27,11 @@ import { MediaItemFormComponent } from './media-item-form.component';
   ],
   bootstrap: [
     AppComponent
-  ]
+  ],
+  providers: [
+    {
+      provide: 'lookupListToken', useValue: lookupLists
+    }
+  ],
 })
 export class AppModule {}
